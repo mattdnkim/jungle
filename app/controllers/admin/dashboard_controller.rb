325c44093ce4b6d23fa6a4c54276d79bf6f26e6f
matt kim <mattkim@matts-MacBook-Air.local>
2022-02-04 14:27:15 -0700

    set admin username and password, using env variable

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,3 +2,7 @@ class Admin::DashboardController < ApplicationController
   def show
   end
 end
+
+class ApplicationController < ActionController::Base
+  http_basic_authenticate_with :name => ENV["HTTP_AUTH_USER"], :password => ENV["HTTP_AUTH_USER"]
+end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -38,3 +38,7 @@ class Admin::ProductsController < ApplicationController
   end
 
 end
+
+class ApplicationController < ActionController::Base
+  http_basic_authenticate_with :name => ENV["HTTP_AUTH_USER"], :password => ENV["HTTP_AUTH_USER"]
+end

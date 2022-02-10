7ea810e785aa39d2eac2bd02ecfaa7b6239ec339
matt kim <mattkim@matts-MacBook-Air.local>
2022-02-09 21:05:30 -0700

    product testing feature

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "see the detail" do
+  visit root_path
+  visit '/product'
+  save_screenshot
+  
+  end
+
+
+end
